c29759908b414e6a4edd27218380f1f2ecb1872b
Henri BALAMOU <henri.balamou@gmail.com>
2025-12-15 14:53:07 +0100

    Update deployment script path and remove obsolete script

--- lamsade_fulldeployement.sh
+++ lamsade_fulldeployement.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
-"${SCRIPT_DIR}/work/scripts/lamsade_fulldeployement.sh" "$@"
+"${SCRIPT_DIR}/work/scripts/lamsade/lamsade_fulldeployement.sh" "$@"
